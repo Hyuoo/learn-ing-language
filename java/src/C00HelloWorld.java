a89195514ddc211eef8463be89ef2ef1ccefe548
Hyungwoo Lim <43870227+Hyuoo@users.noreply.github.com>
2024-03-22 17:53:57 +0900

    java project dir init

---
+++ java/src/C00HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class C00HelloWorld {
+    // main 메서드가 프로그램 메인.
+    // "C00HelloWorld 클래스"를 실행시킨다는 느낌
+    public static void main(String[] args) {
+        System.out.print("안 헬로,, ");
+        System.out.println("Hello, World,,");
+    }
+}
---
+++ java/src/C01AccessModifier.java
@@ -0,0 +1,15 @@
+public class C01AccessModifier {
+    /** Access Modifier (접근 제어자)
+     * public과 같이 클래스나 메서드에 대한 액세스 권한(Accesss Level)을 설정하는 키워드이다.
+     * - Class는 public, default를 사용 할 수 있다.
+     * - attribute, method, constructor는 public, private, default, protected를 사용 할 수 있다.
+     */
+
+    /** (public, protected, default, private)
+     *
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println("asdf");
+    }
+}
